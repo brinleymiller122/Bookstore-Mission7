501b278a91dd5b76e150e7a3614fd8e21ce76e39
brinleymiller122 <miller.brinley@gmail.com>
2022-03-02 16:11:04 -0700

    mission 9

---
+++ Bookstore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket cart; 
+
+        public CartSummaryViewComponent (Basket cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+    
+}
